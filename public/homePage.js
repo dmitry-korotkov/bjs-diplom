a0feaf8737a896c411f726483a785260958d410c
Dmitry Korotkov <korotkov.dmitriy10.1@gmail.com>
2020-02-27 00:14:49 +0400

    tasks of show profile and rates complete

--- public/homePage.js
+++ public/homePage.js
@@ -6,4 +6,27 @@ userLogout.action = () => ApiConnector.logout( response => {
     if(response.success) {
         location.reload();
     }
-} ) 
+});
+
+ApiConnector.current(response => {
+    if (response.success) {
+        ProfileWidget.showProfile(response.data);
+    }
+});
+
+const currientRates = new RatesBoard();
+
+function getRates () {
+    ApiConnector.getStocks(response => {
+        if(response.success) {
+            currientRates.clearTable();
+            currientRates.fillTable(response.data);
+        }
+    });
+} 
+
+getRates();
+
+setInterval(getRates, 60000);
+
+
